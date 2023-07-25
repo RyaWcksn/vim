cb6a87a7de97a248e5797ff0c871093f3b3e97ca
RyaWcksn <pram.aryawcksn@protonmail.ch>
2023-07-25 12:01:10 +0700

    update

--- lua/configs/dap/init.lua
+++ lua/configs/dap/init.lua
@@ -12,3 +12,24 @@ dap.listeners.before.event_exited["dapui_config"] = function()
 end
 vim.fn.sign_define("DapBreakpoint", { text = "B", texthl = "", linehl = "", numhl = "" })
 vim.fn.sign_define("DapStopped", { text = "S", texthl = "", linehl = "", numhl = "" })
+
+
+dap.configurations.scala = {
+  {
+    type = "scala",
+    request = "launch",
+    name = "RunOrTest",
+    metals = {
+      runType = "runOrTestFile",
+      --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+    },
+  },
+  {
+    type = "scala",
+    request = "launch",
+    name = "Test Target",
+    metals = {
+      runType = "testTarget",
+    },
+  },
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -49,6 +49,56 @@ require("lazy").setup({
 		cmd = { "LspInstall", "LspUninstall" },
 		dependencies = "mason.nvim",
 	},
+	{
+		'scalameta/nvim-metals',
+		config = function()
+			local api = vim.api
+			local metals = require("metals")
+			local metals_config = metals.bare_config()
+			metals_config.settings = {
+				showImplicitArguments = true,
+			}
+			metals_config.init_options.statusBarProvider = "on"
+			metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local dap = require("dap")
+			dap.configurations.scala = {
+				{
+					type = "scala",
+					request = "launch",
+					name = "RunOrTest",
+					metals = {
+						runtType = "runOrTestFile",
+					},
+				},
+				{
+					type = "scala",
+					request = "launch",
+					name = "Test Target",
+					metals = {
+						runtType = "testTarget",
+					},
+				},
+			}
+
+			metals_config.on_attach = function(client, bufnr)
+				metals.setup_dap()
+			end
+
+			vim.keymap.set("n", "<leader>lmc", function()
+				require("telescope").extensions.metals.commands()
+			end)
+
+			local nvim_metals_group = api.nvim_create_augroup("metals", { clear = true })
+			api.nvim_create_autocmd("FileType", {
+				pattern = { "scala", "sbt", "java" },
+				callback = function()
+					metals.initialize_or_attach(metals_config)
+				end,
+				group = nvim_metals_group,
+			})
+		end,
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 	-- Lsp
 	{
 		'neovim/nvim-lspconfig',
