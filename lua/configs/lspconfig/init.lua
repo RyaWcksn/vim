71b2e5db044b758401a775031172af806ef060ca
RyaWcksn <pram.aryawcksn@protonmail.ch>
2023-08-07 23:47:49 +0700

    update

--- lua/configs/lspconfig/init.lua
+++ lua/configs/lspconfig/init.lua
@@ -13,37 +13,17 @@ for type, icon in pairs(signs) do
 end
 
 vim.lsp.handlers['window/showMessage'] = function(_, result, ctx)
-  local client = vim.lsp.get_client_by_id(ctx.client_id)
-  local lvl = ({ 'ERROR', 'WARN', 'INFO', 'DEBUG' })[result.type]
-  notify(result.message, lvl, {
-    title = 'LSP | ' .. client.name,
-    timeout = 10000,
-    keep = function()
-      return lvl == 'ERROR' or lvl == 'WARN'
-    end,
-  })
+	local client = vim.lsp.get_client_by_id(ctx.client_id)
+	local lvl = ({ 'ERROR', 'WARN', 'INFO', 'DEBUG' })[result.type]
+	notify(result.message, lvl, {
+		title = 'LSP | ' .. client.name,
+		timeout = 10000,
+		keep = function()
+			return lvl == 'ERROR' or lvl == 'WARN'
+		end,
+	})
 end
 
-function PrintDiagnostics(opts, bufnr, line_nr, client_id)
-	bufnr = bufnr or 0
-	line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
-	opts = opts or { ['lnum'] = line_nr }
-
-	local line_diagnostics = vim.diagnostic.get(bufnr, opts)
-	if vim.tbl_isempty(line_diagnostics) then return end
-
-	local diagnostic_message = ""
-	for i, diagnostic in ipairs(line_diagnostics) do
-		diagnostic_message = diagnostic_message .. string.format("%d: %s", i, diagnostic.message or "")
-		print(diagnostic_message)
-		if i ~= #line_diagnostics then
-			diagnostic_message = diagnostic_message .. "\n"
-		end
-	end
-	vim.api.nvim_echo({ { diagnostic_message, "Normal" } }, false, {})
-end
-
-vim.cmd [[ autocmd! CursorHold * lua PrintDiagnostics() ]]
 
 local function goto_definition(split_cmd)
 	local util = vim.lsp.util
