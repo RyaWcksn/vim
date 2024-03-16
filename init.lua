6c2a598fcee6c0b99c3af0f09b1286db480099dc
RyaWcksn <pram.aryawcksn@gmail.com>
2024-03-17 04:37:37 +0700

    update

--- init.lua
+++ init.lua
@@ -1,8 +1,5 @@
 require('plugins')
 require('options')
+require('options.autocmd')
 require('mappings')
 require('options.statusline').setup()
-
-
-local current_filename = vim.api.nvim_buf_get_name(0)
-print(current_filename)
